d3624c886694c3778cad02280d545b78bf8d36f1
luckify <luckin@fastmail.com>
2024-05-05 10:33:05 +0800

    support macOS / tvOS / visionOS

--- build-ios-xcframework.sh
+++ build-ios-xcframework.sh
@@ -7,7 +7,7 @@ target=$(cd "$(dirname "$0")";pwd)
 [ -d "${target}/out" ] && rm -rf "${target}/out"
 
 build_openssl_lib() {
-    ./Configure enable-tls1_3 $@
+    ./Configure enable-tls1_3 no-tests no-shared $@
 
     make clean
     make -j
@@ -52,6 +52,9 @@ cleanup() {
     rm -rf $@
 }
 
+build_openssl_lib --prefix="${target}/out/macos-arm64_x86_64/OpenSSL.framework" \
+                    apple-universal-macosx
+
 build_openssl_lib --prefix="${target}/out/ios-arm64/OpenSSL.framework" \
                     apple-universal-ios
 
@@ -61,17 +64,44 @@ build_openssl_lib --prefix="${target}/out/ios-arm64_x86_64-simulator/OpenSSL.fra
 build_openssl_lib --prefix="${target}/out/ios-arm64_x86_64-maccatalyst/OpenSSL.framework" \
                     apple-universal-maccatalyst
 
+build_openssl_lib --prefix="${target}/out/tvos-arm64/OpenSSL.framework" \
+                    apple-universal-tvos
+
+build_openssl_lib --prefix="${target}/out/tvos-arm64_x86_64-simulator/OpenSSL.framework" \
+                    apple-universal-tvossim
+
+build_openssl_lib --prefix="${target}/out/xros-arm64/OpenSSL.framework" \
+                    apple-universal-visionos
+
+build_openssl_lib --prefix="${target}/out/xros-arm64_x86_64-simulator/OpenSSL.framework" \
+                    apple-universal-visionossim
+
+build_openssl_framework "${target}/out/macos-arm64_x86_64/OpenSSL.framework"
 build_openssl_framework "${target}/out/ios-arm64/OpenSSL.framework"
 build_openssl_framework "${target}/out/ios-arm64_x86_64-simulator/OpenSSL.framework"
 build_openssl_framework "${target}/out/ios-arm64_x86_64-maccatalyst/OpenSSL.framework"
+build_openssl_framework "${target}/out/tvos-arm64/OpenSSL.framework"
+build_openssl_framework "${target}/out/tvos-arm64_x86_64-simulator/OpenSSL.framework"
+build_openssl_framework "${target}/out/xros-arm64/OpenSSL.framework"
+build_openssl_framework "${target}/out/xros-arm64_x86_64-simulator/OpenSSL.framework"
 
 xcodebuild -create-xcframework \
+           -framework "${target}/out/macos-arm64_x86_64/OpenSSL.framework" \
            -framework "${target}/out/ios-arm64/OpenSSL.framework" \
            -framework "${target}/out/ios-arm64_x86_64-simulator/OpenSSL.framework" \
            -framework "${target}/out/ios-arm64_x86_64-maccatalyst/OpenSSL.framework" \
+           -framework "${target}/out/tvos-arm64/OpenSSL.framework" \
+           -framework "${target}/out/tvos-arm64_x86_64-simulator/OpenSSL.framework" \
+           -framework "${target}/out/xros-arm64/OpenSSL.framework" \
+           -framework "${target}/out/xros-arm64_x86_64-simulator/OpenSSL.framework" \
            -output \
            "${target}/out/OpenSSL.xcframework"
 
-cleanup "${target}/out/ios-arm64" \
+cleanup "${target}/out/macos-arm64_x86_64" \
+        "${target}/out/ios-arm64" \
         "${target}/out/ios-arm64_x86_64-simulator" \
-        "${target}/out/ios-arm64_x86_64-maccatalyst"
+        "${target}/out/ios-arm64_x86_64-maccatalyst" \
+        "${target}/out/tvos-arm64" \
+        "${target}/out/tvos-arm64_x86_64-simulator" \
+        "${target}/out/xros-arm64" \
+        "${target}/out/xros-arm64_x86_64-simulator"
